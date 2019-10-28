015de51f4c9048d88de2751de76b322d6d8d8d74
Fernando_eFirma <fernando@efirma.com>
2019-10-28 17:25:45 -0600

    FASTA parser 0.1

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='FASTA_parser',
+      version='0.1',
+      description='A first try on FASTA files parsing',
+      classifiers=[
+        'Development Status :: Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Bioinformatics :: FASTA',
+        ],
+      keywords='FASTA files parser',
+      url='http://github.com/',
+      author='Fernando Garcia',
+      author_email='dfgr0316@gmail.com',
+      license='MIT',
+      packages=['FASTA_parser'],
+      install_requires = ["biopython >= 1.72",],
+      include_package_data=True,
+      zip_safe=False,
+      entry_points = {
+        'console_scripts': ['is_fasta=FASTA_parser.command_line:main'],
+      },
+      )
